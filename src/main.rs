2703b1bc18dcfe7d0e9c4fa305e58f32c2f951aa
Jamie Melton <jasm1@st-andrews.ac.uk>
2025-07-27 23:08:34 +0100

    Integrated a db

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,60 @@
 #[macro_use] extern crate rocket;
 
 use rocket::data::{Data, ToByteUnit};
+use rocket::fairing::AdHoc;
 use rocket::http::Status;
-use rocket::response::content::{self, RawText};
-use rocket::tokio::fs::File;
+use rocket::response::content;
+use rocket::{Build, Rocket, State};
+use serde::{Serialize, Deserialize};
+use sqlx::SqlitePool;
+use tokio::io::AsyncReadExt;
 
+//create a paste struct
+#[derive(sqlx::FromRow, Serialize, Deserialize)]
+struct Paste {
+    id: String,
+    content: String
+}
 
+async fn run_migrations(rocket: Rocket<Build>) -> Result<Rocket<Build>, Rocket<Build>> {
+    // make a connection to the db
+    let pool = SqlitePool::connect("sqlite://pastes.db").await.unwrap();
+    sqlx::migrate!().run(&pool).await.unwrap();
+    
+    // pool is past to all state handlers 
+    Ok(rocket.manage(pool))
+}
 
 // create a dynamic route
 #[get("/pastes/<id>")]
-async fn show_paste(id: String) -> Option<content::RawText<String>> {
-    let path = format!("uploads/{}", id);
-    let content = rocket::tokio::fs::read_to_string(&path).await.ok()?;
+async fn show_paste(id: String, db: &State<SqlitePool>) -> Option<content::RawText<String>> {
+    // retrive the content from the db
+    let paste = 
+        sqlx::query_as::<_, Paste>("SELECT * FROM pastes WHERE id = ?")
+            .bind(&id)
+            .fetch_one(db.inner())
+            .await
+            .ok()?;
 
-    Some(RawText(content))
+    Some(content::RawText(paste.content))
 }
 
 // create a post request
 #[post("/upload", data = "<paste>")]
-async fn upload(paste: Data<'_>) -> Result<String, Status> {
+async fn upload(paste: Data<'_>, db: &State<SqlitePool>) -> Result<String, Status> {
     // return an id with 8 unique characters
     let id = nanoid::nanoid!(8);
-    let path = format!("uploads/{}", id);
+    
+    let mut content = String::new();
+    let mut stream = paste.open(32.kibibytes());
 
-    if !std::path::Path::new("uploads").exists() {
-        std::fs::create_dir("uploads").map_err(|_| Status::InternalServerError)?;
-    }
+    stream.read_to_string(&mut content).await.map_err(|_| Status::BadRequest)?;  
 
-    let mut file = File::create(&path).await.map_err(|_| Status::InternalServerError)?;
-
-    paste.open(32.kibibytes())
-        .stream_to(&mut file)
+    // execute query
+    sqlx::query("INSERT INTO pastes (id, content) VALUES (?, ?)")
+        .bind(&id)
+        .bind(&content)
+        .execute(db.inner())
         .await
         .map_err(|_| Status::InternalServerError)?;
 
@@ -40,5 +64,7 @@ async fn upload(paste: Data<'_>) -> Result<String, Status> {
 // launch rocket
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![show_paste, upload])
+    rocket::build()
+        .attach(AdHoc::try_on_ignite("SQLx Migrations", run_migrations))
+        .mount("/", routes![show_paste, upload])
 }
